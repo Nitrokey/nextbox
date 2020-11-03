99a9e3eabce7bcfa245b12c4a30945ddae6bb721
Markus Meissner <coder@safemailbox.de>
2020-11-03 08:48:09 +0100

    js boilerplate, 1st assemble_xyz, render_xyz

--- nextbox/js/script.js
+++ nextbox/js/script.js
@@ -1,116 +1,297 @@
 
-// request comm-token (asap, thus here)
 var token = null;
-$.ajax({
-	url: "token", 
-	type: "GET"
-}).done(function(resp) {
-	token = resp.token;
-}).fail(function(respone, code) {
-	token = null;
-});
+var token_created = null;
+var token_max_age = 55 * 5 * 1e3;
+var progress_interval = null;
 
-
-function storage_overview() {
-
-	  var url = "http://192.168.10.129:18585/storage?token=" + token;
+function request(url, method, on_done) {
+	let req = function() {
 		$.ajax({
-    		url: url,
-		    type: "GET",
-		    contentType: "application/json"
-		    //data: JSON.stringify(note)
-
-		}).done(function (response) {
-			
-				set_content(response.toString()); 
-			  //console.log([response]);
-			
-		}).fail(function (response, code) {
-		    // handle failure
-			  console.log([response, code]);
-
+			url: url + "?token=" + token,
+			type: method, 
+			ContentType: "application/json"
+		})
+		.done(on_done)
+		.fail(function(resp, code) {
+			console.error([url, resp, code]);
 		});
+	};
+	
+	// refresh token, if needed
+	let token_age = Math.abs(token_created - (new Date()));
+	if (!token || token_age > token_max_age)
+		request_token(req);
+	else
+		req();
 }
 
 
+function request_token(on_done) {
+	$.ajax({
+		url: "token", 
+		type: "GET"
+	}).done(function(resp) {
+		token = resp.token;
+		token_created = new Date();
+		
+		if (on_done)
+			on_done();
+
+	}).fail(function(resp, code) {
+		token = null;
+		token_created = null;
+		console.error(["failed init token", resp, code]);
+	});
+}
+
+// request comm-token (asap, thus here)
+request_token();
+
+
 function set_content(details, items) {
+	$("#app-content-wrapper").html(details);
 
-	/*let l = "";
-	for (
+	// click event for menu-toggle
+	$(".app-content-list-item-menu > div.icon-more").click(function(ev) {
+		ev.target.parentElement.children[1].classList.toggle("open");
+	});
+
+	// click event for umount
+	$("button.icon-close").click(function(ev) {
+		let name = $(ev.target).parents(".app-content-list-item").first()
+			.children(".app-content-list-item-line-two")
+			.text().split("/")[2];
+
+		request(`http://192.168.10.129:18585/storage/umount/${name}`, "GET", function(resp) {
+			render_storage();
+		});
+	});
+
+	// click mount-name text field
+	$("input.menu-mount-text").click(function(ev) {
+		$(ev.target).val("");
+	});
+
+	// click event for mount
+	$("input.menu-mount-submit").click(function(ev) {
+		// @todo: check name for validity
+		let name = $(ev.target).parent().first().children("input[type=text]").val();
+		let dev = $(ev.target).parents(".app-content-list-item").first()
+			.children(".app-content-list-item-details")
+			.text().split("/")[2];
+
+		request(`http://192.168.10.129:18585/storage/mount/${dev}/${name}`, "GET", function(resp) {
+			render_storage();
+		});
+		return false;
+	});
+
+	// click event for backup, fire backup-start & delay reload for 1000ms
+	$("button.start-backup").click(function(ev) {
+		request("http://192.168.10.129:18585/backup/start", "GET", function(resp) {
+			$("button.start-backup").delay(1000, render_backup);
+		});
+	});
+
+	// timeout reload for backup progress-bar
+	if ($("progress.backup-progress").length > 0) {
+		if(progress_interval === null) {
+			progress_interval = window.setInterval(function() {
+				request("http://192.168.10.129:18585/overview", "GET", function(resp) {
+					if (resp.backup.running) {
+						$("progress.backup-progress").val(resp.backup.progress);
+						let step = (resp.backup.step === undefined) ? "init" : resp.backup.step;
+						$("span.backup-progress-step").text(`${resp.backup.what} step: ${step}`);
+						let percent = resp.backup.progress;
+						if (percent == undefined || percent === null)
+							percent = "0.0";
+						$("span.backup-progress-percent").text(percent + "%");
+					} else {
+						$("progress.backup-progress").val(100);
+						$("span.backup-progress-step").text(`${resp.backup.state}`);
+						$("span.backup-progress-percent").text("100.0%");
+						window.clearInterval(progress_interval);
+						progress_interval = null;
+					}
+				});
+			}, 1000);
+		}
+	} else {
+		if (progress_interval !== null) {
+			window.clearInterval(progress_interval);
+			progress_interval = null;
+		}
+	}
+
+}
+
+function render_list(headline, data) {
+
+	let pre = `<div class="app-content-list">`;
+	if (headline)
+		pre += `<h2>${headline}</h2>`;
+	let post = `</div>`;
+
+	let inner = data.map(function(item) {
+		out = `<a href="#" class="app-content-list-item">`;
+		let icon = item.icon ? item.icon : "?";
+		let bg_col = (item.bg && item.bg !== null) ? item.bg : "rgb(225, 225, 225)";
+		out += `<div class="app-content-list-item-icon" style="background-color: ${bg_col};">${icon}</div>`;
+		out += `<div class="app-content-list-item-line-one">${item.one}</div>`;
+		if (item.two)
+			out += `<div class="app-content-list-item-line-two">${item.two}</div>`;
+		if (item.details)
+			out += `<span class="app-content-list-item-details">${item.details}</span>`;
+		/** add options menu here **/
+		if(item.menu) {
+				out += `<div class="app-content-list-item-menu"><div class="icon-more"></div><div class="popovermenu"><ul>`;
+				
+				out += item.menu.map(function(menu_item) {
+					let tag_class = (menu_item.cls) ? `${menu_item.cls} ` : "";
+					tag_class += `icon-${menu_item.icon} `;
+					if (menu_item.input_value) {
+						tag_class += "icon";
+						return `<li><span class="menuitem"><span class="${tag_class}"></span><form><input type="text" value="${menu_item.input_value}" class="menu-mount-text"><input type="submit" value=" " class="primary icon-checkmark-white menu-mount-submit"></form></span></li>`;
+					} else {
+						return `<li><button class="${tag_class}"><span>${menu_item.name}</span></button></li>`;
+					}
+				}).join("");
+
+				out += `</ul></div></div>`;
+		}
+		out += "</a>";
+		return out;
+	}).join("");
+	return pre + inner + post;
+}
+
+function assemble_storage(data, is_mounted) {
+
+	let mounted = data.mounted;
+	let mounted_devs = Object.keys(data.mounted);
+	return data.available
+		.filter(dev => !is_mounted || (is_mounted && mounted_devs.includes(dev))) 
+		.map(dev => {
+			
+			let ret = Object({
+				icon: "HD",
+				two: mounted[dev] || "(not mounted)",
+				details: dev
+			});
+
+			ret.bg = "rgb(100, 155, 155)";
+			if (!is_mounted && mounted_devs.includes(dev))
+				ret.bg = "rgb(220, 220, 220)";
+
+			ret.one = "Extra Storage";
+			if (data.main == dev)
+				ret.one = "Main Storage";
+			else if (data.backup == dev) 
+				ret.one = "Backup Storage"; 
+
+			if (data.main != dev) {
+				if (mounted_devs.includes(dev)) 
+					ret.menu = new Array( new Object({icon: "close", name: "umount"}) );
+				else
+					ret.menu = new Array( new Object({icon: "folder", input_value: "[mount-name]"}) );
+			}
+
+			return ret;
+		});
+}
+
+function assemble_backup_overview(data) {
+
+	let o_icon = "-";
+	let o_bg = "rgb(220, 220, 220)";
+
+	if (data.what) {
+		o_icon = data.what.substr(0, 1).toUpperCase();
+		o_bg = "rgb(225, 125, 125)";
+	}
+
+	let out = [{
+		icon: "B",
+		bg: "rgb(220, 220, 220)",
+		one: "Backup",
+ 		two: "last: " + ((data.last_backup) ? (new Date(data.last_backup*1e3)).toLocaleString() : "n/a"),
+		menu: [{icon: "upload", name: "start backup", cls: "start-backup"}]
+	}, {
+		icon: "R",
+		bg: "rgb(220, 220, 220)",
+		one: "Restore",
+		two: "last: " + ((data.last_restore) ? (new Date(data.last_restore*1e3)).toLocaleString() : "n/a")
+	}];
+
+	
+	let op_info = ("running" in data) ? data : data.operation;
+
+	if (op_info.running) {
+		out.push({
+			icon: "!",
+			bg: "rgb(125, 225, 225)",
+			one: `<span class="backup-progress-step"></span>`,
+			two: `<progress value=0 max=100 class="backup-progress"></progress>`,
+			details: `<span class="backup-progress-percent"></span>`
+		});
+	}
 
 
-	$("#inside-app").html(`
-    <div class="app-content-detail">${details}</div>
-		${details}
-		<div class="app-content-list"></div>
-	`);*/
+	return out;
+}
 
-	$("#app-content-wrapper").html(`
-			<div class="app-content-list">${details}</div>
-	`);
+function assemble_backup_available(data) {
+	return data.map(item => Object({
+			icon: "B",
+			bg: "rgb(125, 225, 225)",
+			one: item.name,
+		  two: new Date(item.created*1e3).toLocaleString(),
+			details: item.size
+	}));
+}
+
+
+function render_overview() {
+	request("http://192.168.10.129:18585/overview", "GET", function(resp) {
+		let backup = render_list("Backup / Restore Overview", assemble_backup_overview(resp.backup));
+		let storage = render_list("Mounted Storage(s)", assemble_storage(resp.storage, true));
+		set_content(backup + storage);
+	});
+}
+
+
+function render_storage() {
+
+	request("http://192.168.10.129:18585/storage", "GET", function(resp) {
+		let mounted = render_list("Mounted Storages",	assemble_storage(resp.data, true));
+		let available = render_list("Available Storages",	assemble_storage(resp.data, false));
+		set_content(mounted + available);
+	});
+}
+
+
+function render_backup() {
+		request("http://192.168.10.129:18585/backup", "GET", function(resp) {
+		let backup = render_list("Backup / Restore Overview", assemble_backup_overview(resp.data));
+		let available = render_list("Available Backups", assemble_backup_available(resp.data.found));
+		set_content(backup + available);
+	});
+}
+
+function render_dyndns() {
+
 
 }
 
 
 $(function() {
 
+	$("#nav_overview").click(render_overview);
+	$("#nav_storage").click(render_storage);
+	$("#nav_backup").click(render_backup);
+	$("#nav_dyndns").click(render_dyndns);
 
-	//$("#inside-app").text("isodjoidfsj");
-
-	$("#nav_storage").click(storage_overview);
-
-
-	$.ajax({
-		url: "http://192.168.10.129:18585/overview",
-		type: "GET",
-		contentType: "application/json"
-	}).done(function(resp) {
-
-		let details = "<h2>NextBox Overview</h2>"
-		details += "<h1>Backup / Restore Status</h1>";
-		if(resp["backup"]["running"] === false) 
-			details += `<a href="#" class="app-content-list-item">
-            <div class="app-content-list-item-icon" style="background-color: rgb(41, 97, 156);">N</div>
-            <div class="app-content-list-item-line-one">Backup / Restore</div>
-            <div class="app-content-list-item-line-two">not running</div>
-            <!--span class="app-content-list-item-details">8 hours ago</span-->
-            <!--div class="icon-delete"></div-->
-        </a>`;
-		else
-			details += `<a href="#" class="app-content-list-item">
-            <div class="app-content-list-item-icon" style="background-color: rgb(41, 97, 156);">N</div>
-            <div class="app-content-list-item-line-one">Backup / Restore</div>
-            <div class="app-content-list-item-line-two">running</div>
-            <!--span class="app-content-list-item-details">8 hours ago</span-->
-            <!--div class="icon-delete"></div-->
-        </a>`;
-
-		details += "<h1>Mounted Storage(s)</h1>";
-
-		Object.keys(resp["storage"]["mounted"]).map(x => {
-			 let map = resp["storage"]["mounted"];
-				
-			 let what = "Extra Storage";
-			 if (resp["storage"]["main"] == x)
-				what = "Main Storage";
-			 else if (resp["storage"]["backup"] == x)
-				what = "Backup Storage"; 
-
-       details += `
-				<a href="#" class="app-content-list-item">
-            <div class="app-content-list-item-icon" style="background-color: rgb(41, 97, 156);">HD</div>
-            <div class="app-content-list-item-line-one">${what}</div>
-            <div class="app-content-list-item-line-two">${map[x]}</div>
-            <span class="app-content-list-item-details">${x}</span>
-            <!--div class="icon-delete"></div-->
-        </a>
-			 `;
-		})
-
-
-		set_content(details);
-	});
-
+	render_overview();
 
 });
 
--- nextbox/templates/navigation/index.php
+++ nextbox/templates/navigation/index.php
@@ -1,10 +1,10 @@
 
 <ul>
-<li><a href="#" id="nav_storage">USB Storage Mounting</a></li>
+<li><a href="#" id="nav_overview">Overview</a></li>
+<li><a href="#" id="nav_storage">Storage Management</a></li>
 <li><a href="#" id="nav_backup">Backup / Restore</a></li>
 <li><a href="#" id="nav_http">HTTP / HTTPS</a></li>
 <li><a href="#" id="nav_dyndns">Dynamic DNS</a></li>
-<li><a href="#" id="nav_auth">Authorization / Access</a></li>
 </ul>
 
 
