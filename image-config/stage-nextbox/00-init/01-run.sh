02ff735125fbbaa103028867c8a0bfadcc6bf7df
Markus Meissner <coder@safemailbox.de>
2021-03-10 12:16:38 +0100

    comments, usb-quirks, docker-image 1st-boot

--- image-config/stage-nextbox/00-init/01-run.sh
+++ image-config/stage-nextbox/00-init/01-run.sh
@@ -1,19 +1,36 @@
 #!/bin/bash -e
 
+
+#### what default timezone should we set ???
 #echo "${TIMEZONE_DEFAULT}" > "${ROOTFS_DIR}/etc/timezone"
 #rm "${ROOTFS_DIR}/etc/localtime"
 
-# update unattended upgrade to our version
+# modify /boot/cmdline.txt (kernel-cmdline) to use quirks (no UAS, strict usb-storage) for SATA-USB adapter
+sed -i -e 's/rootwait quiet/usb-storage.quirks=0x152d:0x1561:u rootwait quiet/g' "${ROOTFS_DIR}/boot/cmdline.txt"
+# [x] 152d:1561 => 'Sabrent' adapter using 'JMicron Chipset'
+# [ ] 174c:55aa => 'SKL' & 'Inateck' adapters using 'ASMedia Chipset'
+
+
+# unattended upgrades configuration
 install -m 644 ../files/50unattended-upgrades "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
 install -m 644 ../files/20auto-upgrades "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
+
+# docker default configuration, change docker lib-dir to /srv/docker
 install -m 644 ../files/docker-default "${ROOTFS_DIR}/etc/default/docker"
+
+# avahi-daemon service to promote NextCloud as http-service using "nextbox.local"
 install -m 644 ../files/nextcloud.service "${ROOTFS_DIR}/etc/avahi/services/"
+
+# journald-config to keep size limited
 install -m 644 ../files/journald.conf "${ROOTFS_DIR}/etc/systemd/"
 
+# docker images for 1st boot
+mkdir -p "${ROOTFS_DIR}/usr/lib/nextbox-compose/"
+install -m 644 ../files/docker-images.tar "${ROOTFS_DIR}/usr/lib/nextbox-compose/"
 
 on_chroot << EOF
 
-# done by install above, 
+# done by install above, (isn't this bad and overwrites stuff we set before ???)
 systemctl enable unattended-upgrades
 dpkg-reconfigure -f noninteractive unattended-upgrades
 
@@ -23,9 +40,6 @@ echo "/srv/varlog		/var/log		none	defaults,bind		0	0" >> /etc/fstab
 echo "/srv/varcache		/var/cache		none	defaults,bind		0	0" >> /etc/fstab
 
 apt-get install -y ddclient
-
-# disable apache2 autostart not installing certbot-apache anymore!
-#rm /etc/systemd/system/multi-user.target.wants/apache2.service
 
 # add ppa:nitrokey/nextbox repository
 echo "deb http://ppa.launchpad.net/nitrokey/nextbox/ubuntu groovy main" > /etc/apt/sources.list.d/nitrokey-nextbox.list
