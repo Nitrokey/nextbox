dccc461276316917439758dff4c4661253e7edbb
Markus Meissner <coder@safemailbox.de>
2021-04-06 22:56:04 +0200

    update docstrings shield.*

--- nextbox_daemon/shield.py
+++ nextbox_daemon/shield.py
@@ -4,12 +4,18 @@ import gpiozero
 from nextbox_daemon.config import log
 
 class Shield:
+    """
+    Singleton class for the NextBox Reset Board / Shield
+
+    Use only via `shield` global instance after import
+    """
     def __init__(self):
         self.led = gpiozero.RGBLED(19, 20, 21, active_high=False)
         self.button = gpiozero.Button(16, hold_time=5, hold_repeat=False)
         #self.led.on()
 
     def set_led(self, r, g, b):
+        """Switch LED on with `(red, green, blue)` color"""
         self.led.off()
         self.led.on()
 
@@ -19,6 +25,7 @@ class Shield:
 
 
     def set_led_blink(self, r, g, b):
+        """Blink (2s / 50% on) LED on with `(red, green, blue)` color"""
         self.led.off()
 
         col = max(0, min(1, r)), max(0, min(1, g)), max(0, min(1, b))
@@ -27,19 +34,20 @@ class Shield:
 
 
     def set_led_off(self):
+        """Switch LED off"""
         self.led.off()
 
 
     def set_led_state(self, state):
         """
-        Top-level access to LED
+        Top-level access to LED by state
         
-        * 'ready'    => green
-        * 'started'  => yellow
-        * 'updating' => yellow-blinking
-        * 'stopped'  => red
-        * 'factory-reset' => red-blinking
-        * 'button' => blue
+        * 'ready'           => green
+        * 'started'         => yellow
+        * 'updating'        => yellow-blinking
+        * 'stopped'         => red
+        * 'factory-reset'   => red-blinking
+        * 'button'          => blue
         """
 
         if state == "ready":
