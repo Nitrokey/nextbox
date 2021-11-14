08e9fc7f9d3a7dace09e318ac2023e4f2d1d5164
Markus Meissner <coder@safemailbox.de>
2021-11-14 02:56:21 +0100

    api/remote.py cleanups

--- nextbox_daemon/api/remote.py
+++ nextbox_daemon/api/remote.py
@@ -163,17 +163,6 @@ def test_resolve4():
 @remote_api.route("/dyndns/test/reachable")
 @requires_auth
 def test_http():
-    # what = request.path.split("/")[-1]
-    # if what == "proxy":
-    #     domain = cfg["config"]["proxy_domain"]
-    #     what = "https"
-    # else:
-    #     domain = cfg["config"]["domain"]
-    # url = f"{what}://{domain}"
-
-    # if not domain:
-    #     return error("no domain is set")
-
     nc = Nextcloud()
     res, req = nc.check_reachability()
     data = yaml.load(res.text)
@@ -181,10 +170,7 @@ def test_http():
     data["data"]["ipv6"] = ["[" + req["ipv6"] + "]" if req["ipv6"] else "", req["domain"]]
     return success(data["msg"][0], data=data["data"])
     
-    # return error(f"Domain ({what}) test: Not OK",
-    #     data={"exc": "none", "domain": domain})
-
-
+    
 @remote_api.route("/dyndns/test/proxy")
 @requires_auth
 def test_proxy():
@@ -204,56 +190,6 @@ def test_proxy():
         out["nextcloud"] = False
     
     return success("tested proxy for reachability", data=out)
-
-# @remote_api.route("/dyndns/upnp")
-# @requires_auth
-# def setup_upnp():
-#     import netifaces
-#     import upnpclient
-
-#     # get gateway ip
-#     gw_ip = list(netifaces.gateways()['default'].values())[0][0]
-#     # get devices (long operation)
-#     devs = upnpclient.discover(timeout=0.1)
-#     device = None
-#     # filter out gateway
-#     for dev in devs:
-#         if dev._url_base.startswith(f"http://{gw_ip}"):
-#             device = dev
-#             break
-
-#     if device is None:
-#         return error("cannot find upnp-capable router")
-
-#     # check for needed service
-#     service = None
-#     for srv in device.services:
-#         if srv.name == "WANIPConn1":
-#             service = srv
-#             break
-
-#     if service is None:
-#         return error("found upnp-capable router - but w/o the needed service")
-
-#     eth_ip = local_ip()
-
-#     http_args = dict(NewRemoteHost='0.0.0.0', NewExternalPort=80,
-#          NewProtocol='TCP', NewInternalPort=80, NewInternalClient=eth_ip,
-#          NewEnabled='1', NewPortMappingDescription='NextBox - HTTP', NewLeaseDuration=0)
-#     https_args = dict(NewRemoteHost='0.0.0.0', NewExternalPort=443,
-#          NewProtocol='TCP', NewInternalPort=443, NewInternalClient=eth_ip,
-#          NewEnabled='1', NewPortMappingDescription='NextBox - HTTPS',
-#          NewLeaseDuration=0)
-#     service.AddPortMapping(**http_args)
-#     service.AddPortMapping(**https_args)
-
-#     try:
-#         service.GetSpecificPortMappingEntry(**http_args)
-#         service.GetSpecificPortMappingEntry(**https_args)
-#     except upnpclient.soap.SOAPError as e:
-#         return error("failed setting up port-forwarding")
-#     return success("port-forwarding successfully set up")
-
 
 @remote_api.route("/https")
 @requires_auth
@@ -276,58 +212,6 @@ def https_enable():
         "state": "pending",
     })
     job_queue.put("EnableHTTPS")
-
-
-    # domain = request.form.get("domain")
-    # email = request.form.get("email")
-
-    # if not domain or not email:
-    #     return error(f"failed, domain: '{domain}' email: '{email}'")
-
-    # certs = Certificates()
-    # my_cert = certs.get_cert(domain)
-    # if not my_cert:
-    #     log.warning(f"could not get local certificate for: {domain}, acquiring...")
-    #     if not certs.acquire_cert(domain, email):
-    #         msg = f"Failed to acquire {domain} with {email}"
-    #         log.error(msg)
-    #         return error(msg)
-    #     log.info(f"acquired certificate for: {domain} with {email}")
-    #     my_cert = certs.get_cert(domain)
-
-    # certs.write_apache_ssl_conf(
-    #     my_cert["domains"][0], 
-    #     my_cert["fullchain_path"], 
-    #     my_cert["privkey_path"]
-    # )
-
-    # if not certs.set_apache_config(ssl=True):
-    #     return error("failed enabling ssl configuration for apache")
-
-    # log.info(f"activated https for apache using: {domain}")
-
-    # cfg["config"]["https_port"] = 443
-    # cfg["config"]["email"] = email    
-    # cfg.save()
-
-    # # we need to "re-wire" the proxy to port 443 on activated TLS
-    # add_msg = ""
-    # if cfg["config"]["proxy_active"]:
-    #     subdomain = cfg["config"]["proxy_domain"].split(".")[0]
-    #     scheme = "https"
-    #     token = cfg["config"]["nk_token"]
-    #     proxy_tunnel = ProxyTunnel()
-    #     try:
-    #         proxy_port = proxy_tunnel.setup(token, subdomain, scheme)
-    #     except ProxySetupError as e:
-    #         log.error(exc_info=e)
-    #         add_msg = "(but register at proxy-server error: " + repr(e) + ")"
-    #     except Exception as e:
-    #         log.error(exc_info=e)
-    #         add_msg = "(unexpected error during proxy setup)"
-
-    #     cfg["config"]["proxy_port"] = proxy_port
-    #     cfg.save()
 
     return success("HTTPS enabling pending")
 
@@ -377,32 +261,6 @@ def getcerts():
             dct["cert"] = my_cert
     
     return success(data=dct)
-
-
-# @remote_api.route("/certs/acquire", methods=["POST"])
-# @requires_auth
-# def acquire_cert():
-
-#     domain = cfg.get("config", {}).get("domain")
-#     email = cfg.get("config", {}).get("email")
-    
-#     certs = Certificates()
-#     if certs.get_cert(domain):
-#         return error(f"certificate for {domain} already acquired")
-
-#     if len(certs.get_local_certs()) > 0:
-#         certs.clear_certs()
-    
-#     #if not certs.acquire_cert(domain, email):
-#     #    return error(f"could not acquire certificate")
-
-#     board.set("tls", {
-#         "what": "acquire",
-#         "state": "pending",
-#     })
-#     job_queue.put("AcquireCertificate")
-
-#     return success(f"acquiring certificate for {domain} in progress")
 
 @remote_api.route("/certs/clear", methods=["POST"])
 @requires_auth
