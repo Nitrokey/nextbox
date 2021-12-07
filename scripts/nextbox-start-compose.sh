9436b8b21f9585b0f332008d618ad764f169baad
Markus Meissner <coder@safemailbox.de>
2021-12-07 17:24:07 +0100

    update Nextcloud to 21.0.7

--- scripts/nextbox-start-compose.sh
+++ scripts/nextbox-start-compose.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
 
-if (grep '21.0.2' /srv/nextcloud/version.php); then 
-	/usr/bin/docker-compose -f docker-compose-badnc.yml up
-else 
-	/usr/bin/docker-compose -f docker-compose.yml up
-fi
+/usr/bin/docker-compose -f docker-compose.yml up
 
 
--- scripts/nextbox-stop-compose.sh
+++ scripts/nextbox-stop-compose.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
 
-if (grep '21.0.2' /srv/nextcloud/version.php); then 
-	/usr/bin/docker-compose -f docker-compose-badnc.yml down -v
-else 
-	/usr/bin/docker-compose -f docker-compose.yml down -v
-fi
+/usr/bin/docker-compose -f docker-compose.yml down -v
 
 
