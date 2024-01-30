9f9825c7727dff8edad12e410e63aa59c50eb23b
nitrosimon <simon@nitrokey.com>
2024-01-30 11:06:44 +0100

    fixed nextcloud not stopping

--- daemon/scripts/nextbox-update-debian.sh
+++ daemon/scripts/nextbox-update-debian.sh
@@ -1,51 +1,49 @@
 #!/bin/bash
 
 #first of all lets kill all docker instances
-/usr/bin/docker-compose -f docker-compose.yml down -v
-
-
+systemctl stop nextbox-compose.service
 #setting locales
 
-locale-gen --purge en_US.UTF-8
+locale-gen --purge en_US.UTF-8 
 echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\n' > /etc/default/locale
 #prerequisites
-echo "executing prerequisites" &&
+echo "executing prerequisites" 
 
-echo "updating..." &&
-apt update &&
-apt remove apt-listchanges --assume-yes &&
+echo "updating..." 
+apt update 
+apt remove apt-listchanges --assume-yes 
 apt -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" -fuy dist-upgrade
 
-echo "cleanup" &&
-apt  clean &&
-apt -fuy  autoremove &&
+echo "cleanup" 
+apt  clean 
+apt -fuy  autoremove 
 
 
 ##upgrade itself
 
-echo "configure upgrade" &&
+echo "configure upgrade" 
 
-export DEBIAN_FRONTEND=noninteractive &&
-export APT_LISTCHANGES_FRONTEND=none &&
+export DEBIAN_FRONTEND=noninteractive 
+export APT_LISTCHANGES_FRONTEND=none 
 
-echo "executing buster to bullseye" &&
-sudo sed -i 's#/debian-security bullseye/updates# bullseye-security#g' /etc/apt/sources.list
+echo "executing buster to bullseye" 
+sed -i 's#/debian-security bullseye/updates# bullseye-security#g' /etc/apt/sources.list
 sed -i 's:buster/updates:bullseye-security:' /etc/apt/sources.list
 sed -i 's:buster/updates:bullseye-security:' /etc/apt/sources.list.d/*.list
-sudo sed -i 's/buster/bullseye/g' /etc/apt/sources.list
-sudo sed -i 's/buster/bullseye/g' /etc/apt/sources.list.d/*.list
-sudo sed -i 's#/debian-security bullseye/updates# bullseye-security#g' /etc/apt/sources.list
+sed -i 's/buster/bullseye/g' /etc/apt/sources.list
+sed -i 's/buster/bullseye/g' /etc/apt/sources.list.d/*.list
+sed -i 's#/debian-security bullseye/updates# bullseye-security#g' /etc/apt/sources.list
 
 
 
-echo "upgrade..." &&
+echo "upgrade..." 
 
-apt update &&
-apt  -o Dpkg::Options::="--force-confold"  -o Dpkg::Options::="--force-confdef" -fuy upgrade &&
+apt update 
+apt  -o Dpkg::Options::="--force-confold"  -o Dpkg::Options::="--force-confdef" -fuy upgrade 
 apt  -o Dpkg::Options::="--force-confold"  -o Dpkg::Options::="--force-confdef" -fuy dist-upgrade
 
-echo "cleanup" &&
-apt -fuy  autoremove &&
+echo "cleanup" 
+apt -fuy  autoremove
 
 
 sed -i 's:/usr/lib/dhcpcd5/dhcpcd:/usr/sbin/dhcpcd:' /etc/systemd/system/dhcpcd.service.d/wait.conf
