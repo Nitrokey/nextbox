b569afd2c0912538d8eb0a5c60a29ccdd57d1c7e
blu Grobusch <code@blu-mail.de>
2024-03-18 13:36:34 +0100

    add tag_release as part of release cycle

---
+++ debian/tag_release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+vergt() {
+	[[ `python3 -c "from packaging import version; print(version.parse('$1') > version.parse('$2'))"` == *"True"* ]] && [[ "$1" != "$2" ]]
+	}
+
+last_tag=$(git tag -l --sort=taggerdate | tail -n 1)
+last_full_version=$(head -n 1 debian/changelog | grep -Eo '[0-9\.\-]*')
+last_version=$(echo $last_full_version | cut -d "-" -f 1)
+
+if [[ `git branch --show-current` != "master" ]]; then
+	echo "[ERR] Other branch than master detected!"
+	echo "[ERR] Exiting, please make sure to start this script only from master branch!"
+	exit 1
+fi
+
+if vergt ${last_version} ${last_tag}; then
+	echo "[i] adding tag 'v${last_version}'"
+	git tag v${last_version}
+	git push --tags
+	echo "[i] tag 'v${last_version}' successfully added as successor of tag '${last_tag}'!"
+else
+	echo "[ERR] New version 'v${last_version}' incompatible: has to be newer than last tag '${last_tag}'!"
+	exit 1
+fi
