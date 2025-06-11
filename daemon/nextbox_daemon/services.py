9d640910c3cb4bc291de32326b5bb51fb275a400
Markus Meissner <coder@safemailbox.de>
2025-06-11 13:12:40 +0200

    daemon: properly disable userconfig

--- daemon/nextbox_daemon/services.py
+++ daemon/nextbox_daemon/services.py
@@ -2,6 +2,7 @@ from nextbox_daemon.config import log
 from nextbox_daemon.consts import SYSTEMCTL_BIN
 from nextbox_daemon.command_runner import CommandRunner
 
+
 class Services:
     """
     SystemD service/unit handling class
@@ -15,41 +16,54 @@ class Services:
     """
 
     SAFE_SERVICE_CTRL = ["start", "restart", "status", "is-active", "unmask"]
-    FULL_SERVICE_CTRL = SAFE_SERVICE_CTRL + ["stop", "disable", "enable"]
-    
+    FULL_SERVICE_CTRL = SAFE_SERVICE_CTRL + ["stop", "disable", "enable", "mask"]
+
     SERVICES_CTRL = {
         "ddclient": ("ddclient.service", FULL_SERVICE_CTRL),
-
         "nextbox-daemon": ("nextbox-daemon.service", SAFE_SERVICE_CTRL),
         "nextbox-compose": ("nextbox-compose.service", FULL_SERVICE_CTRL),
         "nextbox-updater": ("nextbox-updater.service", SAFE_SERVICE_CTRL),
         "nextbox-factory-reset": ("nextbox-factory-reset.service", SAFE_SERVICE_CTRL),
         "dphys-swapfile": ("dphys-swapfile.service", SAFE_SERVICE_CTRL),
-
         "reverse-tunnel": ("reverse-tunnel.service", FULL_SERVICE_CTRL),
         "apt-daily": ("apt-daily.service", SAFE_SERVICE_CTRL),
         "apt-daily-upgrade": ("apt-daily-upgrade.service", SAFE_SERVICE_CTRL),
+        "userconfig": ("userconfig.service", FULL_SERVICE_CTRL),
     }
 
     # shortcut methods
-    def status(self, name): return self.exec(name, "status")
-    def start(self, name): return self.exec(name, "start")
-    def stop(self, name): return self.exec(name, "stop")
-    def restart(self, name): return self.exec(name, "restart")
-    def enable(self, name): return self.exec(name, "enable")
-    def disable(self, name): return self.exec(name, "disable")
-    def unmask(self, name): return self.exec(name, "unmask")
+    def status(self, name):
+        return self.exec(name, "status")
+
+    def start(self, name):
+        return self.exec(name, "start")
+
+    def stop(self, name):
+        return self.exec(name, "stop")
+
+    def restart(self, name):
+        return self.exec(name, "restart")
+
+    def enable(self, name):
+        return self.exec(name, "enable")
+
+    def disable(self, name):
+        return self.exec(name, "disable")
+
+    def unmask(self, name):
+        return self.exec(name, "unmask")
+
+    def mask(self, name):
+        return self.exec(name, "mask")
 
     # special treatment, just want a boolean here...
-    def is_active(self, name): return self.exec(name, "is-active") \
-        .get("output", " ")[0].strip() == "active"
-    
+    def is_active(self, name):
+        return self.exec(name, "is-active").get("output", " ")[0].strip() == "active"
 
     def check(self, name, op):
         """Check, if given service (name) is allowed to run op(eration)"""
 
-        return name in self.SERVICES_CTRL \
-            and op in self.SERVICES_CTRL[name][1]
+        return name in self.SERVICES_CTRL and op in self.SERVICES_CTRL[name][1]
 
     def exec(self, name, op):
         """Execute 'op(eration)' on service identified with 'name'"""
@@ -65,12 +79,13 @@ class Services:
         # run 'systemctl' command
         cr = CommandRunner([SYSTEMCTL_BIN, op, service], block=True)
         output = [x for x in cr.output if x]
-        
+
         return {
-            "service":     name,
-            "operation":   op,
+            "service": name,
+            "operation": op,
             "return-code": cr.returncode,
-            "output":      output
+            "output": output,
         }
+
 
 services = Services()
