912df4e420adaf6edf2f31f633274d356f0597a5
Jan Knittel (Nitrokey) <jan.knittel@nitrokey.com>
2026-07-09 16:34:12 +0200

    fix: add conditional usage of systemd-resolve or resolvectl depending on debian version
    systemd-resolve was deprecated in debian 12
    
    Signed-off-by: Jan Knittel (Nitrokey) <jan.knittel@nitrokey.com>

--- daemon/nextbox_daemon/consts.py
+++ daemon/nextbox_daemon/consts.py
@@ -42,5 +42,6 @@ DYNDNS_DESEC_REGISTER = "https://desec.io/api/v1/auth/"
 
 
 SYSTEMD_RESOLVE_BIN = "/usr/bin/systemd-resolve"
+SYSTEMD_RESOLVECTL_BIN = "/usr/bin/resolvectl"
 SYSTEMCTL_BIN = "/usr/bin/systemctl"
 
